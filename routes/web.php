f3460137c4a964dd17f62f62fe03cdc34dccefbe
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-02-07 02:01:59 +0100

    Testing mongodb connection to server

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BlehController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('test',  BlehController::class)->names('test');
